baa85b1e90688404145d4caaf6a69f1ff6f9756d
AkylaiTopoeva <akylaytop@gmail.com>
2024-10-08 10:02:50 +0600

    add some tests and change dir for home.html

--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
-    template_name= 'registration/home.html'
+    template_name= 'home.html'
