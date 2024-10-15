650617dafd1584e29b07b89f10ddd53a4987df99
AkylaiTopoeva <akylaytop@gmail.com>
2024-10-15 09:48:13 +0600

    add templates

--- articles/views.py
+++ articles/views.py
@@ -5,18 +5,18 @@ from django.views.generic.edit import DeleteView, UpdateView
 
 class ArticleListView(ListView):
     model = Article
-    template_name = 'article_list.html'
+    template_name = 'articles/article_list.html'
 
 class ArticleDetailView(DetailView):
     model = Article
-    template_name = 'article_detail.html'
+    template_name = 'articles/article_detail.html'
 
 class ArticleUpdateView(UpdateView):
     model = Article
     fields = ( "title", "body", )
-    template_name = 'article_update.html'
+    template_name = 'articles/article_update.html'
 
 class ArticleDeleteView(DeleteView):
     model = Article
-    template_name = 'article_delete.html'
+    template_name = 'articles/article_delete.html'
     success_url = reverse_lazy("article_list")
