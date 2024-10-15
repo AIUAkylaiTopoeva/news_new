c1dc9edc65756b44c6ba1b7257fd2cc9c278fe97
AkylaiTopoeva <akylaytop@gmail.com>
2024-10-15 09:58:20 +0600

    change error, edit to update

--- articles/urls.py
+++ articles/urls.py
@@ -4,7 +4,7 @@ from .views import ArticleListView, ArticleDeleteView, ArticleUpdateView, Articl
 urlpatterns = [
     path("", ArticleListView.as_view(), name="article_list"),
     path("<int:pk>/", ArticleDetailView.as_view(), name="article_detail"), 
-    path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_edit"),
+    path("<int:pk>/edit/", ArticleUpdateView.as_view(), name="article_update"),
     path("<int:pk>/delete/", ArticleDeleteView.as_view(), name="article_delete"),
-    
+
 ]
