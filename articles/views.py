51706c1ee90bb41ed6667c63c32e2fe8d8ff4b21
AkylaiTopoeva <akylaytop@gmail.com>
2024-10-22 09:33:58 +0600

    solve one problem in models, add views for comment

--- articles/views.py
+++ articles/views.py
@@ -1,9 +1,12 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from .models import Article, Comment
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, FormView
 from django.views.generic.edit import DeleteView, UpdateView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .forms import CommentForm
+from django.views import View
+from django.views.generic.detail import SingleObjectMixin
+
 
 class ArticleListView(LoginRequiredMixin, ListView):
     model = Article
@@ -42,11 +45,39 @@ class ArticleCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
     
-class ArticleDetailView(LoginRequiredMixin, DetailView):
+class CommentGet(LoginRequiredMixin, DetailView):
     model = Article
-    template_name = "article_detail.html"   
+    template_name = "articles/article_detail.html"   
 
     def get_context_data(self, **kwargs): 
         context = super().get_context_data(**kwargs)
         context['form'] = CommentForm()
         return context
+    
+class CommentPost(SingleObjectMixin, FormView): 
+    model = Article
+    form_class = CommentForm
+    template_name = "articles/article_detail.html"
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+    
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.article = self.object
+        comment.save()
+        return super().form_valid(form)
+    
+    def get_success_url(self):
+        article = self.get_object()
+        return reverse("article_detail", kwargs={"pk": article.pk})
+
+class ArticleDetailView(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+        view = CommentGet.as_view()
+        return view(request, *args, **kwargs)
+    
+    def post(self, request, *args, **kwargs):
+        view = CommentPost.as_view()
+        return view(request, *args, **kwargs)
